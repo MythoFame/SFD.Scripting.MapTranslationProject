d51559943c18063975aaedafcca3092dae862781
HAZARD <ghostyn678+git@gmail.com>
2026-08-26 03:43:00 -0300

    bootstrap project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using SFDGameScriptInterface;
+
+namespace SFD.Scripting.MapTranslationProject;
+
+public abstract class GameScriptInterfaceExtended : GameScriptInterface
+{
+    protected static readonly IGame Game;
+}
+
+public partial class GameScript : GameScriptInterfaceExtended
+{
+    // ...
+}
